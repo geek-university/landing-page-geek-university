021eccb192005bbeca79f53da6c31cfda0f81724
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-03-19 05:44:39 +0400

    config moved to where it belongs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ LandingPageGeekUniversity::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
