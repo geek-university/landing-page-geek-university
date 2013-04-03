7b6ab5ca224ab82333d42565fa3fc094cd6b5365
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-04-03 05:34:29 +0400

    trying to make media queries for images

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,7 +5,7 @@ $(function ($) {
 
     $email.focus().bind('change focus blur keyup keypress click timer', function () {
         $submit.attr("disabled", !isCorrectEmail($email.val()));
-    })
+    });
 
     setInterval(function () {
         $email.trigger('timer')
@@ -31,46 +31,51 @@ $(function ($) {
 });
 
 $(function ($) {
-    var imgObjects = [
-        {id: "city-far", src: "/assets/city-far2.png", xRange: 40, yRange: 20, invert: true},
-        {id: "footer", src: "/assets/gorodwin-c.png", xRange: 120, yRange: 100,invert: true},
-        {id: "oblako1", src: "/assets/oblako1-15-400.png", xRange: 30, yRange: 30, top: 475, left: 200  },
-        {id: "parashutist", src: "/assets/parashutist-15-150.png", xRange: 10, yRange: 10, top: 80, left: 100  },
-        {id: "vosdushnii-shar", src: "/assets/vosdushnii_shar4.png", xRange: 10, yRange: 10, top: 10, left: 1500  },
-        {id: "about-cloud", src: "/assets/about-cloud3.png", xRange: 30, yRange: 30, top: 350, left: 1230  }
-    ];
-    for (var i = 0; i < imgObjects.length; i++) {
-        var imgObject = imgObjects[i];
+    if (window.innerWidth >= 1500) {
+        var imgObjects = [
+            {id: "city-far", src: "/assets/city-far2.png", xRange: 40, yRange: 20, invert: true},
+            {id: "footer", src: "/assets/gorodwin-c.png", xRange: 140, yRange: 100, invert: true},
+            {id: "oblako1", src: "/assets/oblako1-15-400.png", xRange: 30, yRange: 30, top: 475, left: 200 },
+            {id: "parashutist", src: "/assets/parashutist-15-150.png", xRange: 20, yRange: 20, top: 80, left: 100 },
+            {id: "vosdushnii-shar", src: "/assets/vosdushnii_shar4.png", xRange: 10, yRange: 10, top: 10, left: 1500 },
+            {id: "about-cloud", src: "/assets/about-cloud3.png", xRange: 20, yRange: 20, top: 350, left: 1230 }
+        ];
+        for (var i = 0; i < imgObjects.length; i++) {
+            var imgObject = imgObjects[i];
+            var img = new Image();
+            (function (imgObject) {
+                var id = "#" + imgObject.id;
+                $(id).hide();
+                img.onload = function () {
+                    $(id).show();
+                    $(id).prepend($(this)).css({
+                        opacity: 0,
+                        left: imgObject.left,
+                        top: imgObject.top
+                    }).addClass("plax-item")
+                        .plaxify({"xRange": imgObject.xRange, "yRange": imgObject.yRange, invert: true})
+                        .animate({
+                            opacity: 1
+                        }, getRandomInt(1000, 2000), "swing");
+                }
+            }(imgObject));
+
+            img.src = imgObject.src;
+        }
+        //$('#background').plaxify({invert: true})
+        $('#left-eye').plaxify();
+        $('#right-eye').plaxify();
+        $.plax.enable();
+
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+    } else {
+        //shitty code
         var img = new Image();
-        (function (imgObject) {
-            var id = "#" + imgObject.id;
-            $(id).hide();
-            img.onload = function () {
-                $(id).show();
-                $(id).prepend($(this)).css({
-                    opacity: 0,
-                    left: imgObject.left,
-                    top: imgObject.top,
-                    bottom: imgObject.bottom,
-                    width: imgObject.width,
-                    zIndex: imgObject.zIndex,
-                    position: "absolute"
-                }).addClass("plax-item")
-                    .plaxify({"xRange": imgObject.xRange, "yRange": imgObject.yRange, invert: true})
-                    .animate({
-                        opacity: 1
-                    }, getRandomInt(1000, 2000), "swing");
-            }
-        }(imgObject));
-
-        img.src = imgObject.src;
-    }
-    $('#background').plaxify({invert: true})
-    $('#left-eye').plaxify()
-    $('#right-eye').plaxify()
-    $.plax.enable()
-
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
+        img.onload = function() {
+            $("#about-cloud").prepend($(this));
+        };
+        img.src = "/assets/about-cloud3.png";
     }
 });
