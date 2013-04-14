afea7ccda0a6e47c9c07548d1861ffee98c9eeb7
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-04-14 16:16:09 +0400

    some changes

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,8 @@
 $(function ($) {
     var $email = $("#subscription_email");
     var $submit = $("subscribe_button");
+    var $thank_you = $("#thank_you");
+    var $incorrect_email = $("#incorrect-email");
 
     function isCorrectEmail(email) {
         var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b/;
@@ -10,12 +12,17 @@ $(function ($) {
     $submit.click(function () {
         if (isCorrectEmail($email.val())) {
             $.post('/subscriptions', {"subscription[email]": $email.val()}, function (response) {
-                $("#thank-you").fadeIn();
+                $thank_you.fadeIn();
                 $email.val('');
                 setTimeout(function () {
-                    $('#thank-you').fadeOut();
+                    $thank_you.fadeOut();
                 }, 3000);
             });
+        } else {
+            $incorrect_email.fadeIn();
+            setTimeout(function () {
+                $incorrect_email.fadeOut();
+            }, 3000);
         }
         return false;
     });
