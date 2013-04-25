61db61de3f0d01e40f20064a6313c07d43de2006
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2013-04-25 15:28:22 +0400

    remove </script> from yandex.metrika.js

--- app/assets/javascripts/yandex.metrika.js
+++ app/assets/javascripts/yandex.metrika.js
@@ -21,5 +21,3 @@
 		d.addEventListener("DOMContentLoaded", f, false);
 	} else { f(); }
 })(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="//mc.yandex.ru/watch/21095719" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
